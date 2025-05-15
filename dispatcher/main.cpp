40995026f59b8bdf186eb4d2330a346b0eceff76
Maria_K <anansy.pineapple@gmail.com>
2025-05-16 00:22:02 +0300

    copied

---
+++ dispatcher/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include "Airplane.h"
+
+int main()
+{
+	vector<Airplane* > planes;
+	planes.push_back(new SmallPlane());
+	planes.push_back(new MediumPlane());
+	planes.push_back(new TrainingPlane());
+
+	for (int i = 0; i < planes.size(); i++)
+	{
+		planes[i]->printInfo();
+		cout << endl;
+	}
+}
